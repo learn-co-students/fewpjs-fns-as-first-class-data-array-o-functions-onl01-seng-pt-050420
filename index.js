eca2c909ac0e85d2f9f58fb53a9ee98e298dc7fe
ParishBen <ParishBen13@gmail.com>
2020-11-10 19:25:08 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+var wakeDog = function wakeDog(dogName, dogBreed) {
+    return `Wake ${dogName} the ${dogBreed}`;
+}
+
+var leashDog = function leashDog(dogName, dogBreed) {
+    return `Leash ${dogName} the ${dogBreed}`;
+}
+var walkToPark = function walkToPark(dogName, dogBreed){
+    return `Walk to the park with ${dogName} the ${dogBreed}`;
+}
+var throwFrisbee = function throwFrisbee(dogName, dogBreed){
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`;
+}
+var walkHome = function walkHome(dogName, dogBreed){
+    return `Walk home with ${dogName} the ${dogBreed}`;
+}
+var unleashDog = function unleashDog(dogName, dogBreed){
+    return `Unleash ${dogName} the ${dogBreed}`;
+}
+
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+// function exerciseDog(dogName, dogBreed) {
+//     for(let i = 0; i < routine.length; i++){
+//         return routine[i](dogName, dogBreed)
+//     }
+
+// }
+
+function exerciseDog(dog, breed) {
+    return routine.map(funct => funct(dog, breed));
+  }
